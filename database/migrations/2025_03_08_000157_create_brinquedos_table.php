e3de1e4f63231a7cfe23e093aa9595eb5d3cec25
BERNARDO MARQUETTI <bernardo.060404@bento.local>
2025-03-07 21:03:53 -0300

    implementando brinquedos

---
+++ database/migrations/2025_03_08_000157_create_brinquedos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brinquedos', function (Blueprint $table) {
+            $table->id(); // campo id padrão (chave primária)
+            $table->string('codigo_unico')->unique(); // código único
+            $table->string('nome'); // nome do brinquedo
+            $table->string('tipo'); // tipo do brinquedo
+            $table->string('marca'); // marca do brinquedo
+            $table->date('data_aquisicao'); // data da aquisição
+            $table->decimal('valor_locacao', 8, 2); // valor da locação
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brinquedos');
+    }
+};
