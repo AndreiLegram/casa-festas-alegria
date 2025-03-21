f094bb8455932267f505c8519b183e02a3659c53
BERNARDO MARQUETTI <bernardo.060404@bento.local>
2025-03-21 19:38:47 -0300

    finalizado fluxo de locacoes

--- app/Http/Controllers/LocacoesController.php
+++ app/Http/Controllers/LocacoesController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Locacao; 
-use App\Models\Cliente; 
 
 class LocacoesController extends Controller {
 
@@ -28,40 +27,117 @@ class LocacoesController extends Controller {
     public function store(Request $request)
     {
         $validated = $request->validate([
-            'cpf' => 'required|string|max:15',
-            'nome' => 'required|string|max:255'
+            'codigo' => 'required|string|max:255',
+            'data' => 'required|date',
+            'id_contato' => 'required|exists:clientes,id',
+            'valor_total' => 'required|numeric',
+            'data_devolucao' => 'required|date',
+            'itens_locacao' => 'array',
+            'itens_locacao.*.id_brinquedo' => 'required|exists:brinquedos,id',
+            'itens_locacao.*.quantidade' => 'required|integer|min:1',
+            'itens_locacao.*.valor_unitario' => 'required|integer',
         ]);
 
-        $cliente = Cliente::create($validated); 
-        return response()->json($cliente, 201); 
+        $locacao = Locacao::create([
+            'codigo' => $validated['codigo'],
+            'data' => $validated['data'],
+            'id_contato' => $validated['id_contato'],
+            'valor_total' => $validated['valor_total'],
+            'data_devolucao' => $validated['data_devolucao'],
+        ]);
+
+        if (isset($validated['itens_locacao']) && count($validated['itens_locacao']) > 0) {
+            foreach ($validated['itens_locacao'] as $item) {
+                $locacao->locacaoItems()->create([
+                    'id_brinquedo' => $item['id_brinquedo'],
+                    'quantidade' => $item['quantidade'],
+                    'valor_unitario' => $item['valor_unitario'],
+                    'id_locacao' => $locacao->id,
+                ]);
+            }
+        } else {
+            return response()->json('Locação sem itens!', 400);
+        }
+
+        return response()->json($locacao->load('locacaoItems'), 201);
     }
 
     public function update(Request $request, $id)
     {
-        $cliente = Cliente::find($id);
-
-        if (!$cliente) {
-            return response()->json(['message' => 'cliente não encontrado'], 404);
-        }
-
         $validated = $request->validate([
-            'cpf' => 'required|string|max:15',
-            'nome' => 'required|string|max:255'
+            'codigo' => 'required|string|max:255',
+            'data' => 'required|date',
+            'id_contato' => 'required|exists:clientes,id',
+            'valor_total' => 'required|numeric',
+            'data_devolucao' => 'required|date',
+            'itens_locacao' => 'array',
+            'itens_locacao.*.id_brinquedo' => 'required|exists:brinquedos,id',
+            'itens_locacao.*.quantidade' => 'required|integer|min:1',
+            'itens_locacao.*.valor_unitario' => 'required|integer',
         ]);
 
-        $cliente->update($validated); 
-        return response()->json($cliente); 
+        $locacao = Locacao::find($id);
+
+        if (!$locacao) {
+            return response()->json(['error' => 'Locação não encontrada'], 404);
+        }
+
+        $locacao->update([
+            'codigo' => $validated['codigo'],
+            'data' => $validated['data'],
+            'id_contato' => $validated['id_contato'],
+            'valor_total' => $validated['valor_total'],
+            'data_devolucao' => $validated['data_devolucao'],
+        ]);
+
+        $locacao->locacaoItems()->delete();
+
+        if (isset($validated['itens_locacao']) && count($validated['itens_locacao']) > 0) {
+            foreach ($validated['itens_locacao'] as $item) {
+                $locacao->locacaoItems()->create([
+                    'id_brinquedo' => $item['id_brinquedo'],
+                    'quantidade' => $item['quantidade'],
+                    'valor_unitario' => $item['valor_unitario'],
+                    'id_locacao' => $locacao->id,
+                ]);
+            }
+        } else {
+            return response()->json('Locação sem itens!', 400);
+        }
+
+        return response()->json($locacao->load('locacaoItems'), 200);
     }
+
+    public function pagamento(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'valor_total' => 'required|numeric',
+            'data_pagamento' => 'required|date'
+        ]);
+
+        $locacao = Locacao::find($id);
+
+        if (!$locacao) {
+            return response()->json(['error' => 'Locação não encontrada'], 404);
+        }
+
+        $locacao->update([
+            'data_pagamento' => $validated['data_pagamento']
+        ]);
+
+        return response()->json('Locação paga com sucesso!', 200);
+    }
+
 
     public function destroy($id)
     {
-        $cliente = Cliente::find($id);
+        $locacao = Locacao::find($id);
 
-        if (!$cliente) {
-            return response()->json(['message' => 'cliente não encontrado'], 404);
+        if (!$locacao) {
+            return response()->json(['message' => 'Locacao não encontrado'], 404);
         }
 
-        $cliente->delete();
-        return response()->json(['message' => 'cliente deletado com sucesso']);
+        $locacao->delete();
+        return response()->json(['message' => 'Locacao deletado com sucesso']);
     }
 }
